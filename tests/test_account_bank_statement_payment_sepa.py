ef8aaa83404933bc120a2816f1c47fd2d2f08e83
resteve <resteve@zikzakmedia.com>
2015-12-01 10:55:50 +0100

    Tests

--- tests/test_account_bank_statement_payment_sepa.py
+++ tests/test_account_bank_statement_payment_sepa.py
@@ -1,29 +1,18 @@
-#!/usr/bin/env python
-# This file is part of account_bank_statement_payment_sepa module for Tryton.
+# This file is part of the account_bank_statement_payment_sepa module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountBankStatementPaymentSEPATestCase(unittest.TestCase):
-    'Test Account Bank Statement Payment SEPA module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_bank_statement_payment_sepa')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountBankStatementPaymentSepaTestCase(ModuleTestCase):
+    'Test Account Bank Statement Payment Sepa module'
+    module = 'account_bank_statement_payment_sepa'
 
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        AccountBankStatementPaymentSEPATestCase))
+        AccountBankStatementPaymentSepaTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
